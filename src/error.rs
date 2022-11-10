2ed184b2d090cea56af631450455bc9bad32abd6
文博浩 <wenbohao@sics.ac.cn>
2022-11-10 11:03:09 +0800

    Remove thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,51 +1,71 @@
 //! Error definitions.
 
 use std::collections::TryReserveError;
-use thiserror::Error;
+use std::fmt;
 
 /// A type alias for `Result<T, Error>`.
 pub type Result<T> = std::result::Result<T, Error>;
 
 /// An error that can be returned when uses date/time types.
-#[derive(Error, Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Error {
-    #[error("(full) year must be between 1 and 9999")]
     DateOutOfRange,
-    #[error("(full) hour must be between 0 and 23")]
     TimeOutOfRange,
-    #[error("the leading precision of the interval is too small")]
     IntervalOutOfRange,
-    #[error("invalid number")]
     InvalidNumber,
-    #[error("not a valid month")]
     InvalidMonth,
-    #[error("day of month must be between 1 and last day of month")]
     InvalidDay,
-    #[error("minutes must be between 0 and 59")]
     InvalidMinute,
-    #[error("seconds must be between 0 and 59")]
     InvalidSecond,
-    #[error("the fractional seconds must be between 0 and 999999")]
     InvalidFraction,
-    #[error("date not valid for month specified")]
     InvalidDate,
-    #[error("numeric overflow")]
     NumericOverflow,
-    #[error("divisor is equal to zero")]
     DivideByZero,
-    #[error("{0}")]
     InvalidFormat(String),
-    #[error("{0}")]
     FormatError(String),
-    #[error("{0}")]
     ParseError(String),
-    #[error("{0}")]
     TryReserveError(TryReserveError),
 }
 
-impl From<std::fmt::Error> for Error {
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match &self {
+            Error::TryReserveError(ref e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Error::DateOutOfRange => write!(f, "(full) year must be between 1 and 9999"),
+            Error::TimeOutOfRange => write!(f, "(full) hour must be between 0 and 23"),
+            Error::IntervalOutOfRange => {
+                write!(f, "the leading precision of the interval is too small")
+            }
+            Error::InvalidNumber => write!(f, "invalid number"),
+            Error::InvalidMonth => write!(f, "not a valid month"),
+            Error::InvalidDay => write!(f, "day of month must be between 1 and last day of month"),
+            Error::InvalidMinute => write!(f, "minutes must be between 0 and 59"),
+            Error::InvalidSecond => write!(f, "seconds must be between 0 and 59"),
+            Error::InvalidFraction => {
+                write!(f, "the fractional seconds must be between 0 and 999999")
+            }
+            Error::InvalidDate => write!(f, "date not valid for month specified"),
+            Error::NumericOverflow => write!(f, "numeric overflow"),
+            Error::DivideByZero => write!(f, "divisor is equal to zero"),
+            Error::InvalidFormat(ref e) => write!(f, "{}", e),
+            Error::FormatError(ref e) => write!(f, "{}", e),
+            Error::ParseError(ref e) => write!(f, "{}", e),
+            Error::TryReserveError(ref e) => write!(f, "{}", e),
+        }
+    }
+}
+
+impl From<fmt::Error> for Error {
     #[inline]
-    fn from(e: std::fmt::Error) -> Self {
+    fn from(e: fmt::Error) -> Self {
         match try_format!("{}", e) {
             Ok(s) => Error::FormatError(s),
             Err(e) => e,
